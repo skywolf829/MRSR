c1fed66f829fe94a42b5f4640d244df2b8f124c8
Skylar Wurster <swwurster@gmail.com>
2021-01-10 21:32:23 -0500

    Update npy2h5.py

--- npy2h5.py
+++ npy2h5.py
@@ -2,29 +2,20 @@ import numpy as np
 import h5py 
 import os
 
-MVTVSSR_folder_path = os.path.dirname(os.path.abspath(__file__))
+FlowSTSR_folder_path = os.path.dirname(os.path.abspath(__file__))
+FlowSTSR_folder_path = os.path.join(FlowSTSR_folder_path, "InputData", "iso512")
 
-
-for filename in os.listdir(MVTVSSR_folder_path):
+for filename in os.listdir(FlowSTSR_folder_path):
     if(".npy" in filename):
         print("Removing " + filename)
-        f = np.load(os.path.join(MVTVSSR_folder_path, filename))
+        f = np.load(os.path.join(FlowSTSR_folder_path, filename))
         f_name = filename.split('.')[0]
         f_name = f_name + ".h5"
         hf = h5py.File(f_name, 'w')
         hf.create_dataset('data',data=f)
         hf.close()
         print("Created " + f_name)
-        os.remove(os.path.join(MVTVSSR_folder_path, filename))
-        print("Deleted " + os.path.join(MVTVSSR_folder_path, filename))
+        os.remove(os.path.join(FlowSTSR_folder_path, filename))
+        print("Deleted " + os.path.join(FlowSTSR_folder_path, filename))
     else:
         print("Skipping " + filename)
-
-
-for filename in os.listdir(MVTVSSR_folder_path):
-    if(".h5" in filename):
-        print("Opening " + filename)
-        f = h5py.File(filename, 'r')
-        a = f.get('data')
-        f.close()
-        print("Closing " + filename)
