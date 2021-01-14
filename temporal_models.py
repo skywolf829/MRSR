638ac2ff1a835b96dfb3afefe9c4c501e6fb6381
Skylar Wurster <swwurster@gmail.com>
2021-01-13 21:20:55 -0500

    Update temporal_models.py

--- temporal_models.py
+++ temporal_models.py
@@ -292,7 +292,7 @@ class ConvLSTM(nn.Module):
             layer_output_list.append(layer_output)
             last_state_list.append([h, c])
 
-        return layer_output[:,-1,:,:,:,:]
+        return layer_output[-1,:,:,:,:].unsqueeze()
 
     def _init_hidden(self, shape):
         init_states = []
