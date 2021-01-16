645e2b827bd55b29b2d5df73b2c0a3dbd6c038dd
Skylar Wurster <swwurster@gmail.com>
2021-01-15 19:23:25 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,2 +1,28 @@
-from pyJHTDB import test_plain
-test_plain()
+import os
+import imageio
+import argparse
+from typing import Union, Tuple
+import numpy as np
+import zeep
+import struct
+import base64
+import time
+import h5py
+from concurrent.futures import ThreadPoolExecutor, as_completed
+client = zeep.Client('http://turbulence.pha.jhu.edu/service/turbulence.asmx?WSDL')
+token="edu.osu.buckeyemail.wurster.18-92fb557b" #replace with your own token
+
+result=client.service.GetAnyCutoutWeb(token,"isotropic1024coarse", "u", 100,
+                                            1+1, 1+1, 
+                                            1+1, 100, 100, 100,
+                                            1, 1, 1, 0, "")  # put empty string for the last parameter
+# transfer base64 format to numpy
+nx=int(100)
+ny=int(100)
+nz=int(100)
+base64_len=int(nx*ny*nz*num_components)
+base64_format='<'+str(base64_len)+'f'
+
+result=struct.unpack(base64_format, result)
+result=np.array(result).reshape((nz, ny, nx, num_components))
+print(result.shape)
