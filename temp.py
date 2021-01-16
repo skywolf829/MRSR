556a5c728770e1d67b00aa3fbcbf25a3d09a8a16
Skylar Wurster <swwurster@gmail.com>
2021-01-15 19:24:29 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -13,8 +13,8 @@ client = zeep.Client('http://turbulence.pha.jhu.edu/service/turbulence.asmx?WSDL
 token="edu.osu.buckeyemail.wurster.18-92fb557b" #replace with your own token
 
 result=client.service.GetAnyCutoutWeb(token,"isotropic1024coarse", "u", 100,
-                                            1+1, 1+1, 
-                                            1+1, 100, 100, 100,
+                                            1, 1, 
+                                            1, 100, 100, 100,
                                             1, 1, 1, 0, "")  # put empty string for the last parameter
 # transfer base64 format to numpy
 nx=int(100)
