b79807c505671082cb732cc79038b7e07b82a50f
Skylar Wurster <swwurster@gmail.com>
2021-01-20 17:20:45 -0500

    Updates quadtree code

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import json
-from json import JSONEncoder
+from json import JSONEncoder, JSONDecoder
 from utility_functions import *
 
 class NumpyArrayEncoder(JSONEncoder):
@@ -12,20 +12,19 @@ class NumpyArrayEncoder(JSONEncoder):
             return obj.tolist()
         elif isinstance(obj, torch.Tensor):
             return obj.cpu().numpy().tolist()
-        return JSONEncoder.default(self, obj)
+        return NumpyArrayEncoder.default(self, obj)
+
+class TorchArrayDecoder(JSONDecoder):
+    def default(self, obj, device="cuda"):
+        if isinstance(obj, list):
+            return torch.from_numpy(np.array(obs)).to(device)
+        return TorchArrayDecoder.default(self, obj)
 
 def MSE(x, GT):
     return ((x-GT)**2).mean()
 
 def PSNR(x, GT, max_diff=255.0):
     return 20 * torch.log(torch.tensor(max_diff)) - 10*torch.log(MSE(x, GT))
-
-#old
-def up(img, x_start, x_end, y_start, y_end, skip):
-    for x in range(x_start, x_end, skip):
-        for y in range(y_start, y_end, skip):
-            img[x:x+skip,y:y+skip,:] = img[x,y,:]
-    return img
 
 def upsample_from_quadtree_start(quadtree,device="cuda"):
     full_img = torch.zeros(quadtree['full_shape']).to(device)
@@ -52,13 +51,23 @@ def upsample_from_quadtree(quadtree,full_img,device="cuda"):
             int((y-quadtree['y_start'])/upscale_factor), :]
     return full_img
 
+def pointwise_upscaling(img):
+    upscaled_img = torch.zeros([int(img.shape[0]*2), int(img.shape[1]*2), 
+    img.shape[2]]).to(img.device)
+
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            upscaled_img[x*2:(x+1)*2, y*2:(y+1)*2,:] = img[x,y,:]
+    return upscaled_img
 
 def upscale_from_quadtree_debug(quadtree,max_stride=8,device="cuda"):
     full_img = torch.zeros([int(quadtree['full_shape'][0]/max_stride),
     int(quadtree['full_shape'][1]/max_stride), quadtree['full_shape'][2]]).to(device)
-    
     curr_stride = max_stride
-    while(curr_stride > 1):
+    z = np.log(max_stride) / np.log(2)
+    while(curr_stride > 0):
+        x = np.log(curr_stride) / np.log(2)
+        c = torch.tensor([1.0-(x/z), 1.0-(x/z), 1.0-(x/z)]).to(device)
         # 1. Fill in known data
         trees = [quadtree]
         while(len(trees) > 0):
@@ -70,32 +79,17 @@ def upscale_from_quadtree_debug(quadtree,max_stride=8,device="cuda"):
                     int(curr_tree['y_start']/curr_stride): \
                     int(curr_tree['y_start']/curr_stride)+int((curr_tree['data'].shape[1]*curr_tree['stride'])/curr_stride),
                     :
-                ] = torch.tensor([curr_stride, curr_stride, curr_stride]).to(device)
+                ] = c
             elif(len(curr_tree['children']) > 0):
                 for i in range(len(curr_tree['children'])):
                     trees.append(curr_tree['children'][i])
 
-        # 2. Upsample
-        full_img = full_img.permute(2,0,1).unsqueeze(0)
-        full_img = F.interpolate(full_img, scale_factor=2, mode='bilinear', align_corners=True)
-        full_img = full_img[0].permute(1,2,0)
+        # 2. Upsample if necessary
+        if(curr_stride > 1):
+            full_img = pointwise_upscaling(full_img)
+        
         curr_stride = int(curr_stride / 2)
 
-    # Fill in known data again
-    trees = [quadtree]
-    while(len(trees) > 0):
-        curr_tree = trees.pop(0)
-        if(curr_tree['data'] is not None and curr_tree['stride'] <= curr_stride):
-            full_img[
-                int(curr_tree['x_start']/curr_stride): \
-                int(curr_tree['x_start']/curr_stride)+int((curr_tree['data'].shape[0]*curr_tree['stride'])/curr_stride),
-                int(curr_tree['y_start']/curr_stride): \
-                int(curr_tree['y_start']/curr_stride)+int((curr_tree['data'].shape[1]*curr_tree['stride'])/curr_stride),
-                :
-            ] =  torch.tensor([curr_stride, curr_stride, curr_stride]).to(device)
-        elif(len(curr_tree['children']) > 0):
-            for i in range(len(curr_tree['children'])):
-                trees.append(curr_tree['children'][i])
     return full_img
 
 def upscale_from_quadtree_start(quadtree,max_stride=8,device="cuda"):
@@ -103,7 +97,7 @@ def upscale_from_quadtree_start(quadtree,max_stride=8,device="cuda"):
     int(quadtree['full_shape'][1]/max_stride), quadtree['full_shape'][2]]).to(device)
     
     curr_stride = max_stride
-    while(curr_stride > 1):
+    while(curr_stride > 0):
         # 1. Fill in known data
         trees = [quadtree]
         while(len(trees) > 0):
@@ -120,55 +114,45 @@ def upscale_from_quadtree_start(quadtree,max_stride=8,device="cuda"):
                 for i in range(len(curr_tree['children'])):
                     trees.append(curr_tree['children'][i])
 
-        # 2. Upsample
-        full_img = full_img.permute(2,0,1).unsqueeze(0)
-        full_img = F.interpolate(full_img, scale_factor=2, mode='bilinear', align_corners=True)
-        full_img = full_img[0].permute(1,2,0)
+        imageio.imwrite(str(curr_stride)+"pre_upsample.jpg", full_img.cpu().numpy())
+        if(curr_stride > 1):
+            # 2. Upsample
+            full_img = full_img.permute(2,0,1).unsqueeze(0)
+            full_img = F.interpolate(full_img, scale_factor=2, mode='bilinear', align_corners=True)
+            full_img = full_img[0].permute(1,2,0)
+            imageio.imwrite(str(curr_stride)+"post_upsample.jpg", full_img.cpu().numpy())
         curr_stride = int(curr_stride / 2)
-
-    # Fill in known data again
-    trees = [quadtree]
-    while(len(trees) > 0):
-        curr_tree = trees.pop(0)
-        if(curr_tree['data'] is not None and curr_tree['stride'] <= curr_stride):
-            full_img[
-                int(curr_tree['x_start']/curr_stride): \
-                int(curr_tree['x_start']/curr_stride)+int((curr_tree['data'].shape[0]*curr_tree['stride'])/curr_stride),
-                int(curr_tree['y_start']/curr_stride): \
-                int(curr_tree['y_start']/curr_stride)+int((curr_tree['data'].shape[1]*curr_tree['stride'])/curr_stride),
-                :
-            ] = curr_tree['data'][::int(curr_stride/curr_tree['stride']),::int(curr_stride/curr_tree['stride']),:]
-        elif(len(curr_tree['children']) > 0):
-            for i in range(len(curr_tree['children'])):
-                trees.append(curr_tree['children'][i])
+                
+    imageio.imwrite(str(curr_stride)+".jpg", full_img.cpu().numpy())
     return full_img
 
-def upscale_from_quadtree(quadtree,full_img,device="cuda"):
-    # Guaranteed to be a leaf node
-    data = quadtree['data']
-    upscale_factor = quadtree['stride']
-    data = data.permute(2,0,1).unsqueeze(0)
-    data = F.interpolate(data, scale_factor=upscale_factor, mode='bilinear', align_corners=True)
-    data = data[0].permute(1,2,0)
-    full_img[quadtree['x_start']:quadtree['x_start']+data.shape[0], 
-        quadtree['y_start']:quadtree['y_start']+data.shape[1],:] = data
-    return full_img
-
-def conditional_downsample_quadtree(img,GT_image,criterion,max_MSE=3.0,min_ssim=0.9,min_PSNR=30,
-max_chunk_size=32,max_stride=8,device="cuda"):
-    # Assume starting with a quadtree(img) that is a leaf node
+def conditional_downsample_quadtree(img,GT_image,criterion,criterion_value,
+min_chunk_size=32,max_stride=8,device="cuda"):
     quadtrees_to_check = [img]
     trees_checked = 0
+    #sequence_of_downsampling = []
+    #sequence_of_downsampling_debug = []
+    #sequence_of_downsampling_debug.append(upscale_from_quadtree_debug(img,max_stride=max_stride,device=device).cpu().numpy())
+    #sequence_of_downsampling.append(upsample_from_quadtree_start(img,device=device).cpu().numpy())
     while(len(quadtrees_to_check) > 0):
         trees_checked += 1
         t = quadtrees_to_check.pop(0)
-        if(t['stride'] < max_stride and (t['data'] is None or t['data'].shape[0] > max_chunk_size)):
-            #print("Checking quadtree in quadrant %i:%i, %i:%i" % (t['x_start'], 
-            #t['x_start']+t['data'].shape[0]*t['stride'], t['y_start'], t['y_start']+t['data'].shape[1]*t['stride']))
 
-            if(len(t['children']) == 0):
-                #print("Splitting into 4 subtrees to check")
-                # if the tree has no children, split it into 4 so they can be checked
+        # Check if we can downsample this leaf node
+        t['stride'] = int(t['stride']*2)
+        original_data = t['data'].clone()
+        downsampled_data = original_data[::2,::2,:].clone()
+        t['data'] = downsampled_data
+        new_img = upscale_from_quadtree_start(img,device=device)
+        
+        # If criterion not met, reset data and stride, and see
+        # if the node is large enough to split into subnodes
+        # Otherwise, we keep the downsample, and add the node back as a 
+        # leaf node
+        if(not criterion(GT_image, new_img, criterion_value)):
+            t['data'] = original_data
+            t['stride'] = int(t['stride'] / 2)
+            if(t['data'].shape[0]*t['stride'] > min_chunk_size):
                 for x_quad_start in range(0, t['data'].shape[0], int(t['data'].shape[0]/2)):
                     for y_quad_start in range(0, int(t['data'].shape[1]), int(t['data'].shape[1]/2)):
                         t_new = {
@@ -180,36 +164,19 @@ max_chunk_size=32,max_stride=8,device="cuda"):
                             "y_start": int(y_quad_start*t['stride'] + t['y_start']),
                             "children": []
                         }
-                        t['children'].append(t_new)
-                        
+                        t['children'].append(t_new)     
+                        quadtrees_to_check.append(t_new)                   
                 t['data'] = None
+                t['stride'] = None
+                t['x_start'] = None
+                t['y_start'] = None
+        else:
+            #sequence_of_downsampling_debug.append(upscale_from_quadtree_debug(img,max_stride=max_stride,device=device).cpu().numpy())
+            #sequence_of_downsampling.append(upsample_from_quadtree_start(img,device=device).cpu().numpy())
+            if(t['stride'] < max_stride):
                 quadtrees_to_check.append(t)
-            else:
-                # if the tree does have children, see if the stride can be increased in any quadtree without going above 
-                # the max mse / min ssim
-                
-                for i in range(len(t['children'])):
-                    #print("Attempting to downscale tree in quadrant %i:%i, %i:%i" % \
-                    #(t['children'][i]['x_start'], 
-                    #t['children'][i]['x_start']+t['children'][i]['data'].shape[0]*t['children'][i]['stride'], 
-                    #t['children'][i]['y_start'], 
-                    #t['children'][i]['y_start']+t['children'][i]['data'].shape[1]*t['children'][i]['stride']))
-
-                    t['children'][i]['stride'] = int( t['children'][i]['stride']*2)
-                    child_original_data = t['children'][i]['data']
-                    child_downsampled_data = child_original_data[::2,::2,:]
-                    t['children'][i]['data'] = child_downsampled_data
-                    new_img = upscale_from_quadtree_start(img,device=device)
-
-                    #print("This downscaled subtree gives MSE %0.03f, SSIM %0.02f" % (m,s))
-                    
-                    if(not criterion(GT_image, new_img)):
-                        #print("Criteria not met")
-                        t['children'][i]['data'] = child_original_data
-                        t['children'][i]['stride'] = int(t['children'][i]['stride'] / 2)
-            
-                    quadtrees_to_check.append(t['children'][i])
-                    
+    #imageio.mimwrite("downsampling_sequence.gif", sequence_of_downsampling, fps=5)
+    #imageio.mimwrite("downsampling_sequence_debug.gif", sequence_of_downsampling_debug, fps=5)
     print("Trees checked: %i" % trees_checked)
     return img            
 
@@ -222,7 +189,11 @@ def psnr_criterion(GT_image, img, min_PSNR=80):
 def mse_criterion(GT_image, img, max_mse=200):
     return MSE(img, GT_image) < max_mse
 
+
+
+
 device="cuda"
+
 snick_gt = torch.from_numpy(imageio.imread("snickers.jpg").astype(np.float32)).to(device)
 snick = {
     "full_shape": snick_gt.shape, 
@@ -232,26 +203,20 @@ snick = {
     "y_start": 0,
     "children": []
     }
-j = json.dumps(snick, cls=NumpyArrayEncoder)
-f = open("snick.json",'w')
-f.write(j)
-f.close()
+torch.save(snick, 'snick.torch')
+'''
 import time
 start_time = time.time()
-snick = conditional_downsample_quadtree(snick,snick_gt,ssim_criterion,max_MSE=3.0,min_ssim=0.6,min_PSNR=60,
-max_chunk_size=32,max_stride=8,device="cuda")
+snick = conditional_downsample_quadtree(snick,snick_gt,psnr_criterion,80,
+min_chunk_size=32,max_stride=8,device="cuda")
 end_time = time.time()
 
+
 print("Conditional downsample took % 0.02f seconds" % (end_time - start_time))
-j = json.dumps(snick, cls=NumpyArrayEncoder)
-f = open("snick_quadtree.json",'w')
-f.write(j)
-f.close()
-
-
+torch.save(snick, "snick_ds.torch")
+'''
+snick = torch.load("snick_ds.torch")
 snick_upscaled = upscale_from_quadtree_start(snick)
-
-print("Final PSNR: %0.02f" % PSNR(snick_gt, snick_upscaled))
 
 snick_upsampled = upsample_from_quadtree_start(snick)
 debug = upscale_from_quadtree_debug(snick)
