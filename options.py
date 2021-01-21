d93f2c615914640fae27578920aa8b4e6ce9ae48
Skylar Wurster <swwurster@gmail.com>
2021-01-21 12:36:30 -0500

    Update options.py

--- options.py
+++ options.py
@@ -7,7 +7,7 @@ class Options():
         # Input info
         opt["mode"]                    = "3D"      # What SinGAN to use - 2D or 3D
         opt["data_folder"]             = "InputData/iso1024"
-        opt['scaling_mode']            = "channel" # magnitude, channel, learned, none
+        opt['scaling_mode']            = None # magnitude, channel, learned, none
         opt['load_data_at_start']      = False
         opt['single_shot']            =  False
         opt["save_folder"]             = "SavedModels"
