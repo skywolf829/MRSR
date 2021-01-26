4bfaa37c88b48e5948d37b507e2fa613ca3881da
Skylar Wurster <swwurster@gmail.com>
2021-01-26 13:11:13 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
-python3 train_spatial_SR.py --save_name SSR --device cuda:0 &
-python3 train_temporal_SR.py --save_name TSRTVD --device cuda:1
+python3 train_spatial_SR.py --save_name SSR 
