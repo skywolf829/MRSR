a71ce6944681e961b0840a32b8d1de3d249f9e21
Skylar Wurster <swwurster@gmail.com>
2021-01-27 17:56:53 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -3,36 +3,45 @@ from utility_functions import AvgPool3D
 import h5py
 import imageio
 import numpy as np
+import matplotlib.pyplot as plt
 
 f = h5py.File('bigboy.h5', 'r')
-data = torch.tensor(f['data']).cuda()
+data = torch.tensor(f['data']).type(torch.FloatTensor).cuda()
 f.close()
 data = data.unsqueeze(0)
-print(data.shape)
 data_mag = torch.linalg.norm(data,axis=1)[0]
-print(data_mag.shape)
 
 data_mag /= data_mag.max()
-img_out = torch.zeros(data_mag.shape).cuda()
+image_out = torch.zeros(data_mag.shape).cuda()
 image_out = image_out.unsqueeze(2)
 image_out = image_out.repeat(1, 1, 3)
-print(image_out.shape)
 
-color_mapping_keys = [0.1, 0.5, 0.9]
-color_mapping_values = [torch.from_numpy(np.array([0, 0, 0])), 
-                        torch.from_numpy(np.array([255, 255, 255])),
-                        torch.from_numpy(np.array([255, 0, 0]))]
+plt.hist(data_mag.cpu().numpy().flatten(), bins=25, density=True, cumulative=True)
+plt.show()
+# black white red
+color_mapping_keys = [0.01, 0.3, 0.6]
+color_mapping_values = [torch.from_numpy(np.array([0.0, 0.0, 0.0])).type(torch.FloatTensor).cuda(), 
+                        torch.from_numpy(np.array([200.0, 200.0, 200.0])).type(torch.FloatTensor).cuda(),
+                        torch.from_numpy(np.array([200.0, 0.0, 0.0])).type(torch.FloatTensor).cuda()]
 
-print((data_mag < 0.1).shape)
+
 image_out[data_mag < color_mapping_keys[0]] = color_mapping_values[0]
 
 for i in range(len(color_mapping_keys)-1):
     ratios = data_mag.clone()
     ratios -= color_mapping_keys[i]
     ratios *= 1 / (color_mapping_keys[i+1] - color_mapping_keys[i])
-    image_out[data_mag >= color_mapping_keys[i] and data_mag < color_mapping_keys[i+1]] = \
-    color_mapping_values[i] * (1-ratios) + color_mapping_values[i+1] * ratios
+    ratios = ratios.type(torch.FloatTensor).cuda()
+    
+    cmap = (1-ratios).view(ratios.shape[0], ratios.shape[0], 1).repeat(1, 1, 3) \
+         * color_mapping_values[i].view(1, 1, 3).repeat(ratios.shape[0], ratios.shape[1], 1) 
+    cmap += color_mapping_values[i+1].view(1, 1, 3).repeat(ratios.shape[0], ratios.shape[1], 1) \
+         * ratios.view(ratios.shape[0], ratios.shape[0], 1).repeat(1, 1, 3)
+    
+    indices = torch.bitwise_and(data_mag >= color_mapping_keys[i],
+    data_mag < color_mapping_keys[i+1])
+    image_out[indices] = cmap[indices]
 
 image_out[data_mag > color_mapping_keys[-1]] = color_mapping_values[-1]
 
-imageio.imwrite("bigboy.png", image_out.cpu().numpy())
+imageio.imwrite("bigboy.jpg", image_out.cpu().numpy())
