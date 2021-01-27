b0e17d31bf47bdd13e9e5fbfd3af882558292669
Skylar Wurster <swwurster@gmail.com>
2021-01-27 18:26:38 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -16,15 +16,35 @@ image_out = torch.zeros(data_mag.shape).cuda()
 image_out = image_out.unsqueeze(2)
 image_out = image_out.repeat(1, 1, 3)
 
-plt.hist(data_mag.cpu().numpy().flatten(), bins=25, density=True, cumulative=True)
-plt.show()
+#plt.hist(data_mag.cpu().numpy().flatten(), bins=25, density=True, cumulative=True)
+#plt.show()
+
+'''
 # black white red
 color_mapping_keys = [0.01, 0.3, 0.6]
 color_mapping_values = [torch.from_numpy(np.array([0.0, 0.0, 0.0])).type(torch.FloatTensor).cuda(), 
                         torch.from_numpy(np.array([200.0, 200.0, 200.0])).type(torch.FloatTensor).cuda(),
                         torch.from_numpy(np.array([200.0, 0.0, 0.0])).type(torch.FloatTensor).cuda()]
-
-
+'''
+'''
+# rainbow               R   O    Y     G    B    I     V     R
+color_mapping_keys = [0.0, 0.08, 0.16, 0.25, 0.3, 0.35, 0.4, 1.0]
+color_mapping_values = [torch.from_numpy(np.array([128, 0.0, 0.0])).type(torch.FloatTensor).cuda(), 
+                        torch.from_numpy(np.array([255.0, 136.0, 0.0])).type(torch.FloatTensor).cuda(),
+                        torch.from_numpy(np.array([255.0, 255.0, 0.0])).type(torch.FloatTensor).cuda(),
+                        torch.from_numpy(np.array([0.0, 255.0, 0.0])).type(torch.FloatTensor).cuda(),
+                        torch.from_numpy(np.array([0.0, 0.0, 255.0])).type(torch.FloatTensor).cuda(),
+                        torch.from_numpy(np.array([0.0, 255.0, 200.0])).type(torch.FloatTensor).cuda(),
+                        torch.from_numpy(np.array([128.0, 76.0, 128.0])).type(torch.FloatTensor).cuda(),
+                        torch.from_numpy(np.array([128.0, 0.0, 0.0])).type(torch.FloatTensor).cuda()]
+'''
+# another             blk       Y     G    B    I     V     R
+color_mapping_keys = [0.0, 0.16, 0.4, 0.6]
+color_mapping_values = [torch.from_numpy(np.array([9.0, 171.0, 166.0])).type(torch.FloatTensor).cuda(), 
+                        torch.from_numpy(np.array([0.0, 0.0, 0.0])).type(torch.FloatTensor).cuda(),
+                        torch.from_numpy(np.array([121.0, 9.0, 9.0])).type(torch.FloatTensor).cuda(),
+                        torch.from_numpy(np.array([255.0, 255.0, 255.0])).type(torch.FloatTensor).cuda(),
+                        ]
 image_out[data_mag < color_mapping_keys[0]] = color_mapping_values[0]
 
 for i in range(len(color_mapping_keys)-1):
