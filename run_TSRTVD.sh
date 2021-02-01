05b6ac221dcaf91f3b0e676a27af477d806ac43d
Skylar Wurster <swwurster@gmail.com>
2021-02-01 11:57:20 -0500

    Update run_TSRTVD.sh

--- run_TSRTVD.sh
+++ run_TSRTVD.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
-python3 train_temporal_SR.py --save_name TSRTVD --train_distributed False --beta_1 0.0 --beta_2 0.999
+python3 train_temporal_SR.py --save_name TSRTVD --train_distributed True --beta_1 0.0 --beta_2 0.999
