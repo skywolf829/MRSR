0a37abec890976be8485450d45e7006bc0d9ade8
Skylar Wurster <swwurster@gmail.com>
2021-02-01 14:07:00 -0500

    Update run_temporal.sh

--- run_temporal.sh
+++ run_temporal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
 python3 train_temporal_SR.py --save_name TSRTVD --train_distributed False --beta_1 0.0 --beta_2 0.999 --temporal_model TSRTVD --device 0 &
-python3 train_temporal_SR.py --save_name T_UNet --train_distributed False --beta_1 0.5 --beta_2 0.999 --temporal_model UNet --device 1 &
+python3 train_temporal_SR.py --save_name T_UNet --train_distributed False --beta_1 0.5 --beta_2 0.999 --temporal_model UNet --device 1 
