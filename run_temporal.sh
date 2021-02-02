f20d536cb91a7935f33152c1eb1d0663eb090e30
Skylar Wurster <swwurster@gmail.com>
2021-02-02 09:25:28 -0500

    Update run_temporal.sh

--- run_temporal.sh
+++ run_temporal.sh
@@ -6,4 +6,5 @@ python3 train_spatial_SR.py --save_name SSR_shuffle_1gpu --train_distributed Fal
 python3 train_spatial_SR.py --save_name SSR_shuffle_CNN --train_distributed False --alpha_2 0 --upsample_mode shuffle --beta_1 0.9 --beta_2 0.0 --device cuda:3 &
 python3 train_spatial_SR.py --save_name SSR_shuffle_streamlines5 --train_distributed False --alpha_6 0.1 --streamline_length 5 --upsample_mode shuffle --beta_1 0.9 --beta_2 0.0 --device cuda:4 &
 python3 train_spatial_SR.py --save_name SSR_shuffle_streamlines5 --train_distributed False --alpha_6 0.1 --streamline_length 10 --upsample_mode shuffle --beta_1 0.9 --beta_2 0.0 --device cuda:5 &
-python3 train_spatial_SR.py --save_name SSR_shuffle_streamlines5 --train_distributed False --alpha_6 0.1 --streamline_length 1 --upsample_mode shuffle --beta_1 0.9 --beta_2 0.0 --device cuda:6
+python3 train_spatial_SR.py --save_name SSR_shuffle_streamlines5 --train_distributed False --alpha_6 0.1 --streamline_length 1 --upsample_mode shuffle --beta_1 0.9 --beta_2 0.0 --device cuda:6 &
+python3 train_spatial_SR.py --save_name SSR_trilin_1gpu --train_distributed False --upsample_mode trilinear --beta_1 0.9 --beta_2 0.0 --device cuda:7 
