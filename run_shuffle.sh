7d07c68fc07bf8dcafdcc6dc86aea4beb4754fd8
Skylar Wurster <swwurster@gmail.com>
2021-02-02 10:29:14 -0500

    run scripts changed

--- run_shuffle.sh
+++ run_shuffle.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
-python3 train_spatial_SR.py --save_name SSR_shuffle --upsample_mode shuffle --beta_1 0.9 --beta_2 0.999
+python3 train_spatial_SR.py --save_name SSR_shuffle_8gpus --upsample_mode shuffle --beta_1 0.9 --beta_2 0.999
