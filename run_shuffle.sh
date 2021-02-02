731f98a092ec2fd7b21126c27bd5a0609e440c00
Skylar Wurster <swwurster@gmail.com>
2021-02-02 09:14:25 -0500

    Update run_shuffle.sh

--- run_shuffle.sh
+++ run_shuffle.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
-python3 train_spatial_SR.py --save_name SSR_shuffle --upsample_mode shuffle
+python3 train_spatial_SR.py --save_name SSR_shuffle --upsample_mode shuffle --beta_1 0.9 --beta_2 0.0
