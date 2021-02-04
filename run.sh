c8a9f07cce53fe0d13afced4231a2f72d649ee77
Skylar Wurster <swwurster@gmail.com>
2021-02-04 18:53:43 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
-python3 train_spatial_SR.py --save_name SSR --train_distributed True --alpha_6 0.1 --streamline_length 5 --upsample_mode shuffle --beta_1 0.9 --beta_2 0.999 
+python3 train_spatial_SR.py --save_name SSR --train_distributed True --alpha_6 0.1 --streamline_length 5 --upsample_mode shuffle --beta_1 0.9 --num_workers 6 --beta_2 0.999 
