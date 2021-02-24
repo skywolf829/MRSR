54506cf03931107331bcca2dd31b40c9ef297964
Skylar Wurster <swwurster@gmail.com>
2021-02-24 13:57:22 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
-python3 train_spatial_SR.py --save_name SSR_mixing_p --train_distributed True --upsample_mode shuffle --beta_1 0.9 --num_workers 6 --beta_2 0.999 --data_folder InputData/mix_p 
+python3 train_spatial_SR.py --save_name SSR_iso_mag --train_distributed True --upsample_mode shuffle --beta_1 0.9 --num_workers 6 --beta_2 0.999 --data_folder InputData/iso_mag 
