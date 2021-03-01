0b4210b0883e8a1e92ee83dd7704ad20e64d955d
Skylar Wurster <swwurster@gmail.com>
2021-02-28 20:46:11 -0500

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
-python3 test_SSR.py --output_file_name mixing_p_3D_2x --save_name mixing3D --scale_factor 2 --testing_method model --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
+python3 test_SSR.py --output_file_name --full_resolution 512 --channels 1 mixing_p_3D_2x --save_name mixing3D --scale_factor 2 --testing_method model --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
 #python3 test_SSR.py --output_file_name isomag3D_4x --save_name model_nostreamlines --scale_factor 4 --testing_method model --print True --device cuda:0 --parallel True
 #python3 test_SSR.py --output_file_name isomag3D_8x --save_name model_nostreamlines --scale_factor 8 --testing_method model --print True --device cuda:0 --parallel True
