9996994e02bef9139ab65f55ee3d4abf4b571111
Skylar Wurster <swwurster@gmail.com>
2021-03-02 21:01:32 -0500

    Update sz_test.py

--- sz_test.py
+++ sz_test.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 
         f_size_kb = os.path.getsize(args['file'] + ".dat.sz") / 1024
 
-        command = "sz -x -f -s " + args['file'] + ".dat.sz" + str(args['dims']) + " " + \
+        command = "sz -x -f -s " + args['file'] + ".dat.sz -" + str(args['dims']) + " " + \
             str(args['nx']) + " " + str(args['ny'])
         if(args['dims'] == 3):
             command = command + " " + str(args['nz'])
