935d38fd85bf8f0d1a45d188865f63eec65050cd
Skylar Wurster <swwurster@gmail.com>
2021-03-03 22:42:10 -0500

    Update run_compression_tests.sh

--- run_compression_tests.sh
+++ run_compression_tests.sh
@@ -7,6 +7,6 @@ export PATH="$HOME/sz/bin:$PATH"
 #python3 sz_test.py --metric mre --start_value 0.001 --end_value 0.1 --value_skip 0.01 --output_folder mag2D_4010_mre
 
 
-python3 mixedLOD_octree.py --folder mix_p --file 1010.h5 --upscaling_technique trilinear --criterion psnr --start_metric 10 --end_metric 100 --metric_skip 10 --output_folder mixing3D_1010_psnr --mode 3D --min_chunk 16 --debug false
-python3 mixedLOD_octree.py --folder mix_p --file 1010.h5 --upscaling_technique model --criterion psnr --start_metric 10 --end_metric 100 --metric_skip 10 --output_folder mixing3D_1010_psnr --mode 3D --min_chunk 16 --debug true
+python3 mixedLOD_octree.py --folder mix_p --file 1010.h5 --downscaling_technique avgpool3D --upscaling_technique trilinear --criterion psnr --start_metric 10 --end_metric 100 --metric_skip 10 --output_folder mixing3D_1010_psnr --mode 3D --min_chunk 16 --debug false
+python3 mixedLOD_octree.py --folder mix_p --file 1010.h5 --downscaling_technique avgpool3D --upscaling_technique model --criterion psnr --start_metric 10 --end_metric 100 --metric_skip 10 --output_folder mixing3D_1010_psnr --mode 3D --min_chunk 16 --debug true
 python3 sz_test.py --metric psnr --start_value 10 --end_value 100 --value_skip 10 --output_folder mixing3D_1010_psnr --dims 3 --nx 512 --ny 512 --nx 512
