20b95462e6919b9cb626a36739182364f9df5dbe
Skylar Wurster <swwurster@gmail.com>
2021-03-04 17:52:47 -0500

    Update mixedLOD_octree.py

--- mixedLOD_octree.py
+++ mixedLOD_octree.py
@@ -1168,6 +1168,7 @@ folder : str, name : str, metric : str, value : float):
 
 
 def sz_decompress_nodelist1(filename : str):
+    print("Decompressing with sz method 1")
     folder_path = os.path.dirname(os.path.abspath(__file__))
     temp_folder = os.path.join(folder_path, "Temp")
     if(not os.path.exists(temp_folder)):
@@ -1183,7 +1184,7 @@ def sz_decompress_nodelist1(filename : str):
     return nodes
 
 def sz_decompress_nodelist2(filename : str):
-    
+    print("Decompressing with sz method 2")
     folder_path = os.path.dirname(os.path.abspath(__file__))
     temp_folder = os.path.join(folder_path, "Temp")
     if(not os.path.exists(temp_folder)):
