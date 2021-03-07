f215d74900d93cf54d525af4033b252c7571da24
Skylar Wurster <swwurster@gmail.com>
2021-03-07 17:26:23 -0500

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
 
-python3 test_SSR.py --output_file_name iso2D_2x_3blocks_32kernels --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag2D_3blocks_32kernels --print True --device cuda:0 --parallel False --data_folder isomag2D --mode 2D 
-python3 test_SSR.py --output_file_name iso2D_2x_3blocks_32kernels --full_resolution 1024 --channels 1 --save_name bilinear --scale_factor 2 --testing_method bilinear --print True --device cuda:0 --data_folder isomag2D --mode 2D 
-python3 test_SSR.py --output_file_name iso2D_2x_3blocks_32kernels --full_resolution 1024 --channels 1 --save_name bicubic --scale_factor 2 --testing_method bicubic --print True --device cuda:0 --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x_3blocks_32kernels --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag2D_3blocks_32kernels --print True --device cuda:0 --parallel False --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x_3blocks_32kernels --full_resolution 1024 --channels 1 --save_name bilinear --scale_factor 2 --testing_method bilinear --print True --device cuda:0 --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x_3blocks_32kernels --full_resolution 1024 --channels 1 --save_name bicubic --scale_factor 2 --testing_method bicubic --print True --device cuda:0 --data_folder isomag2D --mode 2D 
 
-python3 test_SSR.py --output_file_name iso2D_2x --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag2D --print True --device cuda:0 --parallel False --data_folder isomag2D --mode 2D 
-python3 test_SSR.py --output_file_name iso2D_2x --full_resolution 1024 --channels 1 --save_name bilinear --scale_factor 2 --testing_method bilinear --print True --device cuda:0 --data_folder isomag2D --mode 2D 
-python3 test_SSR.py --output_file_name iso2D_2x --full_resolution 1024 --channels 1 --save_name bicubic --scale_factor 2 --testing_method bicubic --print True --device cuda:0 --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag2D --print True --device cuda:0 --parallel False --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x --full_resolution 1024 --channels 1 --save_name bilinear --scale_factor 2 --testing_method bilinear --print True --device cuda:0 --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x --full_resolution 1024 --channels 1 --save_name bicubic --scale_factor 2 --testing_method bicubic --print True --device cuda:0 --data_folder isomag2D --mode 2D 
 
-python3 test_SSR.py --output_file_name iso2D_2x_1blocks_96kernels --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag2D_1blocks_96kernels --print True --device cuda:0 --parallel False --data_folder isomag2D --mode 2D 
-python3 test_SSR.py --output_file_name iso2D_2x_1blocks_96kernels --full_resolution 1024 --channels 1 --save_name bilinear --scale_factor 2 --testing_method bilinear --print True --device cuda:0 --data_folder isomag2D --mode 2D 
-python3 test_SSR.py --output_file_name iso2D_2x_1blocks_96kernels --full_resolution 1024 --channels 1 --save_name bicubic --scale_factor 2 --testing_method bicubic --print True --device cuda:0 --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x_1blocks_96kernels --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag2D_1blocks_96kernels --print True --device cuda:0 --parallel False --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x_1blocks_96kernels --full_resolution 1024 --channels 1 --save_name bilinear --scale_factor 2 --testing_method bilinear --print True --device cuda:0 --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x_1blocks_96kernels --full_resolution 1024 --channels 1 --save_name bicubic --scale_factor 2 --testing_method bicubic --print True --device cuda:0 --data_folder isomag2D --mode 2D 
 
 
-python3 test_SSR.py --output_file_name iso2D_2x_LW --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag2D_LW --print True --device cuda:0 --parallel False --data_folder isomag2D --mode 2D 
-python3 test_SSR.py --output_file_name iso2D_2x_LW --full_resolution 1024 --channels 1 --save_name bilinear --scale_factor 2 --testing_method bilinear --print True --device cuda:0 --data_folder isomag2D --mode 2D 
-python3 test_SSR.py --output_file_name iso2D_2x_LW --full_resolution 1024 --channels 1 --save_name bicubic --scale_factor 2 --testing_method bicubic --print True --device cuda:0 --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x_LW --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag2D_LW --print True --device cuda:0 --parallel False --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x_LW --full_resolution 1024 --channels 1 --save_name bilinear --scale_factor 2 --testing_method bilinear --print True --device cuda:0 --data_folder isomag2D --mode 2D 
+#python3 test_SSR.py --output_file_name iso2D_2x_LW --full_resolution 1024 --channels 1 --save_name bicubic --scale_factor 2 --testing_method bicubic --print True --device cuda:0 --data_folder isomag2D --mode 2D 
 
 #python3 test_SSR.py --output_file_name iso2D_2x_3blocks_32kernels --full_resolution 1024 --channels 1 --save_name model --scale_factor 4 --testing_method model --model_name SSR_isomag2D --print True --device cuda:0 --parallel False --data_folder isomag2D --mode 2D 
 #python3 test_SSR.py --output_file_name iso2D_2x_3blocks_32kernels --full_resolution 1024 --channels 1 --save_name bilinear --scale_factor 4 --testing_method bilinear --print True --device cuda:0 --data_folder isomag2D --mode 2D 
@@ -57,8 +57,11 @@ python3 test_SSR.py --output_file_name iso2D_2x_LW --full_resolution 1024 --chan
 
 ###############################################
 
-#python3 test_SSR.py --output_file_name isomag3D_LW_2x --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag3D_LW --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
-#python3 test_SSR.py --output_file_name isomag3D_LW_2x --full_resolution 1024 --channels 1 --save_name trilinear --scale_factor 2 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
+python3 test_SSR.py --output_file_name isomag3D_2x --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_iso_mag --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
+python3 test_SSR.py --output_file_name isomag3D_2x --full_resolution 1024 --channels 1 --save_name trilinear --scale_factor 2 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
+
+python3 test_SSR.py --output_file_name isomag3D_LW_2x --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag3D_LW --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
+python3 test_SSR.py --output_file_name isomag3D_LW_2x --full_resolution 1024 --channels 1 --save_name trilinear --scale_factor 2 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
 
 #python3 test_SSR.py --output_file_name isomag3D_LW_4x --full_resolution 1024 --channels 1 --save_name model --scale_factor 4 --testing_method model --model_name SSR_isomag3D_LW --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
 #python3 test_SSR.py --output_file_name isomag3D_LW_4x --full_resolution 1024 --channels 1 --save_name trilinear --scale_factor 4 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
