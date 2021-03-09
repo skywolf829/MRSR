7460c6cfce21c7d8a5fe9b5ecc183fe4d130914a
Skylar Wurster <swwurster@gmail.com>
2021-03-09 15:56:18 -0500

    Update run_compression_tests.sh

--- run_compression_tests.sh
+++ run_compression_tests.sh
@@ -13,12 +13,12 @@ export PATH="$HOME/fpzip/bin:$PATH"
 #python3 mixedLOD_octree.py --downscaling_technique avgpool2D --upscaling_technique bicubic --criterion psnr --start_metric 20 --end_metric 100 --metric_skip 5 --output_folder mag2D_4010_psnr_compression --mode 2D --file 4010.h5 --dims 2 --nx 1024 --ny 1024 --use_compressor true --compressor sz --load_existing true
 #python3 mixedLOD_octree.py --downscaling_technique avgpool2D --upscaling_technique bicubic --criterion psnr --start_metric 20 --end_metric 100 --metric_skip 5 --output_folder mag2D_4010_psnr_compression --mode 2D --file 4010.h5 --dims 2 --nx 1024 --ny 1024 --use_compressor true --compressor fpzip --load_existing true
 
-#python3 mixedLOD_octree.py --downscaling_technique avgpool2D --upscaling_technique model --model_name SSR_isomag2D --criterion psnr --start_metric 20 --end_metric 80 --metric_skip 5 --output_folder mag2D_compression --mode 2D --file isomag2D_compressiontest.h5 --dims 2 --nx 1024 --ny 1024 --use_compressor true --compressor sz --load_existing false --debug true --device cuda:0 
-python3 mixedLOD_octree.py --downscaling_technique avgpool2D --upscaling_technique model --model_name SSR_isomag2D --criterion psnr --start_metric 20 --end_metric 80 --metric_skip .5 --output_folder mag2D_compression --mode 2D --file isomag2D_compressiontest.h5 --dims 2 --nx 1024 --ny 1024 --use_compressor true --compressor sz --load_existing false --device cuda:1 --dynamic_downscaling false
+python3 mixedLOD_octree.py --downscaling_technique avgpool2D --upscaling_technique model --model_name SSR_isomag2D --criterion psnr --start_metric 20 --end_metric 80 --metric_skip .5 --output_folder mag2D_compression --mode 2D --file isomag2D_compressiontest.h5 --dims 2 --nx 1024 --ny 1024 --use_compressor true --compressor sz --load_existing false --debug true --device cuda:0 
+#python3 mixedLOD_octree.py --downscaling_technique avgpool2D --upscaling_technique model --model_name SSR_isomag2D --criterion psnr --start_metric 20 --end_metric 80 --metric_skip .5 --output_folder mag2D_compression --mode 2D --file isomag2D_compressiontest.h5 --dims 2 --nx 1024 --ny 1024 --use_compressor true --compressor sz --load_existing false --device cuda:1 --dynamic_downscaling false
 
 #python3 mixedLOD_octree.py --downscaling_technique avgpool2D --upscaling_technique model --criterion psnr --start_metric 20 --end_metric 100 --metric_skip 5 --output_folder mag2D_compression --mode 2D --file 4010.h5 --dims 2 --nx 1024 --ny 1024 --use_compressor true --compressor fpzip --load_existing true --debug true
 
-#python3 sz_test.py --metric psnr --channels 1 --file isomag2D_compressiontest.h5 --start_value 20 --end_value 80 --value_skip 5 --dims 2 --nx 1024 --ny 1024 --output_folder mag2D_compression
+python3 sz_test.py --metric psnr --channels 1 --file isomag2D_compressiontest.h5 --start_value 20 --end_value 80 --value_skip .5 --dims 2 --nx 1024 --ny 1024 --output_folder mag2D_compression
 #python3 zfp_test.py --metric psnr --channels 1 --file isomag2D_compressiontest.h5 --start_bpv 0.5 --end_bpv 16 --bpv_skip 0.5 --dims 2 --nx 1024 --ny 1024 --output_folder mag2D_compression
 
 # 3D iso1024 mag
@@ -26,7 +26,7 @@ python3 mixedLOD_octree.py --downscaling_technique avgpool2D --upscaling_techniq
 
 # 3D mixing dataset
 #python3 mixedLOD_octree.py --downscaling_technique avgpool3D --upscaling_technique model --model_name SSR_mix3D_LW --criterion psnr --start_metric 20 --end_metric 100 --metric_skip 5 --output_folder mix3D_compression --mode 3D --file mixing3D_compressiontest.h5 --dims 3 --nx 128 --ny 128 --nz 128 --use_compressor true --compressor sz --load_existing false --debug true --device cuda:2 &
-#python3 sz_test.py --metric psnr --file 1010.h5 --start_value 20 --end_value 100 --value_skip 5 --dims 3 --nx 512 --ny 512 --nz 512 --output_folder mix3D_1010_psnr_compression
+#python3 sz_test.py --metric psnr --file 1010.h5 --start_value 20 --end_value 80 --value_skip .5 --dims 3 --nx 512 --ny 512 --nz 512 --output_folder mix3D_1010_psnr_compression
 #python3 zfp_test.py --metric psnr --file 1010.h5 --start_bpv 0.5 --end_bpv 16 --bpv_skip 0.5 --dims 3 --nx 512 --ny 512 --nz 512 --output_folder mix3D_1010_psnr_compression
 
 # 3D iso1024 VF
