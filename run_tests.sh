9ba79df0e92ef1ee0f7e8804d9b36dc7a936d1e7
Skylar Wurster <swwurster@gmail.com>
2021-03-11 16:40:14 -0500

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -27,37 +27,25 @@ cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
 
 #############################################
 
-python3 test_SSR.py --output_file_name mixing3D_2x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 2 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
-python3 test_SSR.py --output_file_name mixing3D_2x --full_resolution 512 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_2x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 2 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_2x --full_resolution 512 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
 
-python3 test_SSR.py --output_file_name mixing3D_4x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 4 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
-python3 test_SSR.py --output_file_name mixing3D_4x --full_resolution 512 --channels 1 --save_name model --scale_factor 4 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_4x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 4 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_4x --full_resolution 512 --channels 1 --save_name model --scale_factor 4 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
 
-python3 test_SSR.py --output_file_name mixing3D_8x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 8 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
-python3 test_SSR.py --output_file_name mixing3D_8x --full_resolution 512 --channels 1 --save_name model --scale_factor 8 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_8x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 8 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_8x --full_resolution 512 --channels 1 --save_name model --scale_factor 8 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
 
-python3 test_SSR.py --output_file_name mixing3D_16x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 16 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
-python3 test_SSR.py --output_file_name mixing3D_16x --full_resolution 512 --channels 1 --save_name model --scale_factor 16 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_16x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 16 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_16x --full_resolution 512 --channels 1 --save_name model --scale_factor 16 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
 
-python3 test_SSR.py --output_file_name mixing3D_32x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 32 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
-python3 test_SSR.py --output_file_name mixing3D_32x --full_resolution 512 --channels 1 --save_name model --scale_factor 32 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_32x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 32 --testing_method trilinear --print True --device cuda:0 --data_folder mix_p --mode 3D 
+#python3 test_SSR.py --output_file_name mixing3D_32x --full_resolution 512 --channels 1 --save_name model --scale_factor 32 --testing_method model --model_name SSR_mixing_p --print True --device cuda:0 --parallel True --data_folder mix_p --mode 3D 
 
 ###############################################
 
-#python3 test_SSR.py --output_file_name isomag3D_2x --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_iso_mag --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
-#python3 test_SSR.py --output_file_name isomag3D_2x --full_resolution 1024 --channels 1 --save_name trilinear --scale_factor 2 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
-
-#python3 test_SSR.py --output_file_name isomag3D_LW_2x --full_resolution 1024 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_isomag3D_LW --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
-#python3 test_SSR.py --output_file_name isomag3D_LW_2x --full_resolution 1024 --channels 1 --save_name trilinear --scale_factor 2 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
-
-#python3 test_SSR.py --output_file_name isomag3D_LW_4x --full_resolution 1024 --channels 1 --save_name model --scale_factor 4 --testing_method model --model_name SSR_isomag3D_LW --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
-#python3 test_SSR.py --output_file_name isomag3D_LW_4x --full_resolution 1024 --channels 1 --save_name trilinear --scale_factor 4 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
-
-#python3 test_SSR.py --output_file_name isomag3D_LW_8x --full_resolution 1024 --channels 1 --save_name model --scale_factor 8 --testing_method model --model_name SSR_isomag3D_LW --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
-#python3 test_SSR.py --output_file_name isomag3D_LW_8x --full_resolution 1024 --channels 1 --save_name trilinear --scale_factor 8 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
-
-#python3 test_SSR.py --output_file_name isomag3D_LW_16x --full_resolution 1024 --channels 1 --save_name model --scale_factor 16 --testing_method model --model_name SSR_isomag3D_LW --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
-#python3 test_SSR.py --output_file_name isomag3D_LW_16x --full_resolution 1024 --channels 1 --save_name trilinear --scale_factor 16 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
+python3 test_SSR.py --output_file_name iso3D_2x --full_resolution 512 --channels 1 --save_name model --scale_factor 2 --testing_method model --model_name SSR_iso_mag --print True --device cuda:0 --parallel True --data_folder iso1024mag --mode 3D 
+python3 test_SSR.py --output_file_name iso3D_2x --full_resolution 512 --channels 1 --save_name trilinear --scale_factor 2 --testing_method trilinear --print True --device cuda:0 --data_folder iso1024mag --mode 3D 
 
 #################################################
 
