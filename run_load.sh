73d0efe34bdf364367ae26233ddac6e7781bed36
Skylar Wurster <swwurster@gmail.com>
2021-03-15 09:45:28 -0400

    Update run_load.sh

--- run_load.sh
+++ run_load.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/FlowSTSR
-python3 train_spatial_SR.py --load_from SSR_iso_mag --train_distributed True 
+python3 train_spatial_SR.py --load_from SSR_isoVF --train_distributed True 
