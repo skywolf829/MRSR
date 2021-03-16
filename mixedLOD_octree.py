881a3029587984472eee9426ef844fc8c65f6d6b
Skylar Wurster <swwurster@gmail.com>
2021-03-16 10:11:38 -0400

    Update mixedLOD_octree.py

--- mixedLOD_octree.py
+++ mixedLOD_octree.py
@@ -1792,7 +1792,7 @@ if __name__ == '__main__':
                 for i in range(len(nodes)):
                     del nodes[i].data
                 del nodes
-            print(prof.key_averages().table())
+        print(prof.key_averages().table())
         m += args['metric_skip']
 
     if(os.path.exists(os.path.join(save_folder, "results.pkl"))):
