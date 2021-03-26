69fd3599d0ffbffa1aa534dd50a411a268cab41f
Skylar Wurster <swwurster@gmail.com>
2021-03-26 11:53:01 -0400

    Update frequency_analysis.py

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -19,10 +19,18 @@ parser = argparse.ArgumentParser(description='Test fourier space results')
 parser.add_argument('--datafolder',default="")
 parser.add_argument('--GT',default="isomag3D_compressiontest.nc")
 parser.add_argument('--NN',default="isomag3D_compressiontest_NN.nc")
+parser.add_argument('--octree',default="isomag3D_compressiontest_NN_octree.nc")
 parser.add_argument('--SZ',default="isomag3D_compressiontest_SZ.nc")
 parser.add_argument('--device',default="cuda:0")
 
 args = vars(parser.parse_args())
+
+def get_circle(full_size, radius, shell_size, device):
+    xx = torch.arange(0, full_size[0], dtype=torch.float, device=device).view(-1, 1,).repeat(1, full_size[1])
+    yy = torch.arange(0, full_size[1], dtype=torch.float, device=device).view(1, -1).repeat(full_size[0], 1)
+    circle = (((xx-(full_size[0]/2.0))**2) + ((yy-(full_size[1]/2.0))**2))**0.5
+    circle = torch.logical_and(circle < (radius + int(shell_size/2)), circle > (radius - int(shell_size/2)))
+    return circle
 
 def get_sphere(full_size, radius, shell_size, device):
     xxx = torch.arange(0, full_size[0], dtype=torch.float, device=device).view(-1, 1,1).repeat(1, full_size[1], full_size[2])
@@ -50,6 +58,7 @@ font = {#'font.family' : 'normal',
 plt.rcParams.update(font)
 
 GT_freqs = []
+octree_freqs = []
 NN_freqs = []
 SZ_freqs = []
 
@@ -57,20 +66,28 @@ xs = []
 
 GT = torch.tensor(np.array(Dataset(os.path.join(input_folder, args['GT']), 'r', 
 format="NETCDF4")['velocity magnitude']), device=device)
-GT_fft = fft.fftn(GT, dim=(-3, -2, -1))
+if(len(GT.shape) == 3):
+    GT_fft = fft.fftn(GT, dim=(-3, -2, -1))
+elif(len(GT.shape) == 2):
+    GT_fft = fft.fftn(GT, dim=(-2, -1))
 del GT
-GT_fft = torch.roll(GT_fft, shifts=(GT_fft.shape[0]//2, GT_fft.shape[1]//2, GT_fft.shape[2]//2), dims=(-3, -2, -1))
+if(len(GT_fft.shape) == 3):
+    GT_fft = torch.roll(GT_fft, shifts=(GT_fft.shape[0]//2, GT_fft.shape[1]//2, GT_fft.shape[2]//2), dims=(-3, -2, -1))
+elif(len(GT_fft.shape) == 2):
+    GT_fft = torch.roll(GT_fft, shifts=(GT_fft.shape[0]//2, GT_fft.shape[1]//2), dims=(-2, -1))
 
 n_bins = int(GT_fft.shape[0] / 2)
-shell_size = 3
+shell_size = 7
 full_size = list(GT_fft.shape)
 GT_fft = GT_fft.to("cuda:0")
 device = "cuda:0"
 for i in range(0, n_bins):
     print("Bin " + str(i))
     radius = i*((full_size[0]/2) / n_bins)
-    sphere = get_sphere(full_size, radius, shell_size, device)
-    
+    if(len(GT_fft.shape) == 3):
+        sphere = get_sphere(full_size, radius, shell_size, device)
+    elif(len(GT_fft.shape) == 2):
+        sphere = get_circle(full_size, radius, shell_size, device)
     xs.append(radius)
 
     GT_freqs.append(torch.abs((sphere*GT_fft).real).mean().cpu().numpy().item())
@@ -79,50 +96,100 @@ del GT_fft, sphere
 device = "cuda:1"
 NN = torch.tensor(np.array(Dataset(os.path.join(input_folder, args['NN']), 'r', 
 format="NETCDF4")['velocity magnitude']), device=device)
-NN_fft = fft.fftn(NN, dim=(-3, -2, -1))
+if(len(NN.shape) == 3):
+    NN_fft = fft.fftn(NN, dim=(-3, -2, -1))
+elif(len(NN.shape) == 2):
+    NN_fft = fft.fftn(NN, dim=(-2, -1))
 del NN
-NN_fft = torch.roll(NN_fft, shifts=(NN_fft.shape[0]//2, NN_fft.shape[1]//2, NN_fft.shape[2]//2), dims=(-3, -2, -1))
+if(len(NN_fft.shape) == 3):
+    NN_fft = torch.roll(NN_fft, shifts=(NN_fft.shape[0]//2, NN_fft.shape[1]//2, NN_fft.shape[2]//2), dims=(-3, -2, -1))
+elif(len(NN_fft.shape) == 2):
+    NN_fft = torch.roll(NN_fft, shifts=(NN_fft.shape[0]//2, NN_fft.shape[1]//2), dims=(-2, -1))
+
 NN_fft = NN_fft.to(device)
 for i in range(0, n_bins):
     print("Bin " + str(i))
     radius = i*((full_size[0]/2) / n_bins)
-    sphere = get_sphere(full_size, radius, shell_size, device)
-
+    if(len(NN_fft.shape) == 3):
+        sphere = get_sphere(full_size, radius, shell_size, device)
+    elif(len(NN_fft.shape) == 2):
+        sphere = get_circle(full_size, radius, shell_size, device)
     NN_freqs.append(torch.abs((sphere*NN_fft).real).mean().cpu().numpy().item())
 del NN_fft, sphere
 
 device = "cuda:2"
+OT = torch.tensor(np.array(Dataset(os.path.join(input_folder, args['octree']), 'r', 
+format="NETCDF4")['velocity magnitude']), device=device)
+if(len(OT.shape) == 3):
+    OT_fft = fft.fftn(OT, dim=(-3, -2, -1))
+elif(len(OT.shape) == 2):
+    OT_fft = fft.fftn(OT, dim=(-2, -1))
+del OT
+if(len(OT_fft.shape) == 3):
+    OT_fft = torch.roll(OT_fft, shifts=(OT_fft.shape[0]//2, OT_fft.shape[1]//2, OT_fft.shape[2]//2), dims=(-3, -2, -1))
+elif(len(OT_fft.shape) == 2):
+    OT_fft = torch.roll(OT_fft, shifts=(OT_fft.shape[0]//2, OT_fft.shape[1]//2), dims=(-2, -1))
+
+OT_fft = OT_fft.to(device)
+for i in range(0, n_bins):
+    print("Bin " + str(i))
+    radius = i*((full_size[0]/2) / n_bins)
+    if(len(OT_fft.shape) == 3):
+        sphere = get_sphere(full_size, radius, shell_size, device)
+    elif(len(OT_fft.shape) == 2):
+        sphere = get_circle(full_size, radius, shell_size, device)
+    octree_freqs.append(torch.abs((sphere*OT_fft).real).mean().cpu().numpy().item())
+del OT_fft, sphere
+
+
+device = "cuda:3"
 SZ = torch.tensor(np.array(Dataset(os.path.join(input_folder, args['SZ']), 'r', 
 format="NETCDF4")['velocity magnitude']), device=device)
-SZ_fft = fft.fftn(SZ, dim=(-3, -2, -1))
+if(len(SZ.shape) == 3):
+    SZ_fft = fft.fftn(SZ, dim=(-3, -2, -1))
+elif(len(SZ.shape) == 2):
+    SZ_fft = fft.fftn(SZ, dim=(-2, -1))
 del SZ
-SZ_fft = torch.roll(SZ_fft, shifts=(SZ_fft.shape[0]//2, SZ_fft.shape[1]//2, SZ_fft.shape[2]//2), dims=(-3, -2, -1))
+if(len(SZ_fft.shape) == 3):
+    SZ_fft = torch.roll(SZ_fft, shifts=(SZ_fft.shape[0]//2, SZ_fft.shape[1]//2, SZ_fft.shape[2]//2), dims=(-3, -2, -1))
+elif(len(SZ_fft.shape) == 2):
+    SZ_fft = torch.roll(SZ_fft, shifts=(SZ_fft.shape[0]//2, SZ_fft.shape[1]//2), dims=(-2, -1))
+
 SZ_fft = SZ_fft.to(device)
 for i in range(0, n_bins):
     print("Bin " + str(i))
     radius = i*((full_size[0]/2) / n_bins)
-    sphere = get_sphere(full_size, radius, shell_size, device)
+    if(len(SZ_fft.shape) == 3):
+        sphere = get_sphere(full_size, radius, shell_size, device)
+    elif(len(SZ_fft.shape) == 2):
+        sphere = get_circle(full_size, radius, shell_size, device)
 
     SZ_freqs.append(torch.abs((sphere*SZ_fft).real).mean().cpu().numpy().item())
 del SZ_fft, sphere
 
 fig = plt.figure()
 
-plt.plot(xs, GT_freqs, label="Raw data")
-plt.plot(xs, NN_freqs, label="Ours")
-plt.plot(xs, SZ_freqs, label="SZ")
+plt.plot(xs, np.array(GT_freqs), label="Raw data", color="red")
+plt.plot(xs, np.array(NN_freqs), label="Ours", color="blue")
+plt.plot(xs, np.array(octree_freqs), label="SR-octree", color="gray")
+plt.plot(xs, np.array(SZ_freqs), label="SZ", color="green")
 
-plt.title("Radially Averaged Power Spectrum Density")
+plt.title("Iso2D magnitude")
 plt.ylabel("Power")
 plt.xlabel("Wavenumber")
 plt.legend()
+plt.yscale("log")
+plt.xscale("log")
 print("xs")
 print(xs)
-print("GT_freqs, NN_freqs, and SZ_freqs")
+print("GT_freqs, NN_freqs, octree_freqs, and SZ_freqs")
 print(GT_freqs)
 print()
 print(NN_freqs)
 print()
+print(octree_freqs)
+print()
 print(SZ_freqs)
-#plt.show()
+fig.tight_layout()
+plt.show()
 plt.savefig("powerspectra.png")
