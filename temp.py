df271cd609716390cc5648b5144709ffdf41d685
Skylar Wurster <swwurster@gmail.com>
2021-06-25 13:03:37 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -14,7 +14,7 @@ from netCDF4 import Dataset
 FlowSTSR_folder_path = os.path.dirname(os.path.abspath(__file__))
 
 
-load_folder = os.path.join(FlowSTSR_folder_path, "TrainingData", "0.h5")
+load_folder = os.path.join(FlowSTSR_folder_path, "TrainingData", "Supernova", "0.h5")
 f = h5py.File(load_folder, 'r')
 rootgrp = Dataset("supernova.nc", "w", format="NETCDF4")
 rootgrp.createDimension("x")
