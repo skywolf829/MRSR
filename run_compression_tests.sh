604747f1bea286deba4a7460c6698df1d699031d
Skylar Wurster <swwurster@gmail.com>
2021-06-27 09:32:25 -0400

    Update run_compression_tests.sh

--- run_compression_tests.sh
+++ run_compression_tests.sh
@@ -41,27 +41,27 @@ export PATH="$HOME/sz/bin:$PATH"
 
 
 # Vort dataset
-#python3 -u mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
-#--upscaling_technique model --model_name Vorts --criterion psnr --start_metric 28 \
-#--end_metric 60 --metric_skip 1.0 --output_folder Vorts_compression_test --max_LOD 5 \
-#--min_chunk 4 --mode 3D --file Vorts_compressiontest.h5 --dims 3 --nx 128 --ny 128 \
-#--nz 128 --use_compressor true --distributed false --compressor sz --load_existing false \
-#--save_netcdf true --save_netcdf_octree true --debug true --preupscaling_PSNR true \
-#--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
-
-#python3 -u sz_test.py --metric mre --channels 1 --file Vorts_compressiontest.h5 \
-#--start_value 0.2 --end_value 0.8 --value_skip .01 --dims 3 --nx 128 --ny 128 \
-#--nz 128 --output_folder Vorts_compression_test --save_netcdf false
-
-
-# Plume dataset
 python3 -u mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
---upscaling_technique model --model_name Plume --criterion psnr --start_metric 38 \
---end_metric 60 --metric_skip 100.0 --output_folder Plume_compression_test --max_LOD 5 \
---min_chunk 4 --mode 3D --file Plume_compressiontest.h5 --dims 3 --nx 512 --ny 128 \
+--upscaling_technique model --model_name Vorts --criterion psnr --start_metric 40 \
+--end_metric 60 --metric_skip 100.0 --output_folder Vorts_compression_test --max_LOD 5 \
+--min_chunk 4 --mode 3D --file Vorts_compressiontest.h5 --dims 3 --nx 128 --ny 128 \
 --nz 128 --use_compressor true --distributed false --compressor sz --load_existing true \
 --save_netcdf true --save_netcdf_octree true --debug true --preupscaling_PSNR true \
 --device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
+
+python3 -u sz_test.py --metric mre --channels 1 --file Vorts_compressiontest.h5 \
+--start_value 0.024 --end_value 0.8 --value_skip 1.01 --dims 3 --nx 128 --ny 128 \
+--nz 128 --output_folder Vorts_compression_test --save_netcdf true
+
+
+# Plume dataset
+#python3 -u mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
+#--upscaling_technique model --model_name Plume --criterion psnr --start_metric 38 \
+#--end_metric 60 --metric_skip 100.0 --output_folder Plume_compression_test --max_LOD 5 \
+#--min_chunk 4 --mode 3D --file Plume_compressiontest.h5 --dims 3 --nx 512 --ny 128 \
+#--nz 128 --use_compressor true --distributed false --compressor sz --load_existing true \
+#--save_netcdf true --save_netcdf_octree true --debug true --preupscaling_PSNR true \
+#--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
 
 #python3 -u sz_test.py --metric mre --channels 1 --file Plume_compressiontest.h5 \
 #--start_value 0.143 --end_value 0.960 --value_skip 1.004 --dims 3 --nx 512 --ny 128 \
