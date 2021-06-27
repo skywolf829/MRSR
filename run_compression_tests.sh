b96508a1351ee66837e7b58d821ed6a6bded9e40
Skylar Wurster <swwurster@gmail.com>
2021-06-27 10:01:05 -0400

    Update run_compression_tests.sh

--- run_compression_tests.sh
+++ run_compression_tests.sh
@@ -39,19 +39,32 @@ export PATH="$HOME/sz/bin:$PATH"
 # 3D iso1024 VF
 #python3 mixedLOD_octree.py --downscaling_technique avgpool3D --upscaling_technique model --model_name SSR_iso3D_VF --criterion psnr --start_metric 20 --end_metric 100 --metric_skip 5 --output_folder iso3D_VF_compression --mode 3D --file iso3D_VF_compressiontest.h5 --dims 3 --nx 128 --ny 128 --nz 128 --use_compressor true --compressor sz --load_existing false --debug true --device cuda:3 &
 
-
-# Vort dataset
-python3 -u mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
---upscaling_technique model --model_name Vorts --criterion psnr --start_metric 40 \
---end_metric 60 --metric_skip 100.0 --output_folder Vorts_compression_test --max_LOD 5 \
---min_chunk 4 --mode 3D --file Vorts_compressiontest.h5 --dims 3 --nx 128 --ny 128 \
---nz 128 --use_compressor true --distributed false --compressor sz --load_existing true \
---save_netcdf true --save_netcdf_octree true --debug true --preupscaling_PSNR true \
+# Mixing 2D dataset
+python3 -u mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool2D \
+--upscaling_technique model --model_name Mixing2D --criterion psnr --start_metric 27 \
+--end_metric 60 --metric_skip 1.0 --output_folder Mixing2D_compression_test --max_LOD 7 \
+--min_chunk 8 --mode 2D --file Mixing2D_compressiontest.h5 --dims 2 --nx 1024 --ny 1024 \
+--use_compressor true --distributed false --compressor sz --load_existing false \
+--save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
 --device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
 
-python3 -u sz_test.py --metric mre --channels 1 --file Vorts_compressiontest.h5 \
---start_value 0.024 --end_value 0.8 --value_skip 1.01 --dims 3 --nx 128 --ny 128 \
---nz 128 --output_folder Vorts_compression_test --save_netcdf true
+python3 -u sz_test.py --metric mre --channels 1 --file Mixing2D_compressiontest.h5 \
+--start_value 0.004 --end_value 0.98 --value_skip .004 --dims 2 --nx 1024 --ny 1024 \
+--output_folder Mixing2D_compression_test --save_netcdf false
+
+
+# Vort dataset
+#python3 -u mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
+#--upscaling_technique model --model_name Vorts --criterion psnr --start_metric 40 \
+#--end_metric 60 --metric_skip 100.0 --output_folder Vorts_compression_test --max_LOD 5 \
+#--min_chunk 4 --mode 3D --file Vorts_compressiontest.h5 --dims 3 --nx 128 --ny 128 \
+#--nz 128 --use_compressor true --distributed false --compressor sz --load_existing true \
+#--save_netcdf true --save_netcdf_octree true --debug true --preupscaling_PSNR true \
+#--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
+
+#python3 -u sz_test.py --metric mre --channels 1 --file Vorts_compressiontest.h5 \
+#--start_value 0.024 --end_value 0.8 --value_skip 1.01 --dims 3 --nx 128 --ny 128 \
+#--nz 128 --output_folder Vorts_compression_test --save_netcdf true
 
 
 # Plume dataset
